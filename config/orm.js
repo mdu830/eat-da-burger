6945b1e371852737793e21f1ff7d24b27af638b6
mdu830 <mdu830@yahoo.com>
2021-03-09 11:45:30 -0500

    rewrote orm (similar to CatsApp)

--- config/orm.js
+++ config/orm.js
@@ -1,76 +1,97 @@
-const connection = require("./connection.js");
+const connection = require('./connection.js');
 
-function printQuestionMarks(num) {
-    
-    let arr = [];
+//function to add question marks (?, ?, ?) for mySQL in query
+const printQuestionMarks = (num) => {
+  const arr = [];
 
-    for (const i = 0; i < num; i++) {
-        arr.push("?");
-    }
+  for (let i = 0; i < num; i++) {
+    arr.push('?');
+  }
 
-    return arr.toString();
-}
-
-function objToSql(ob) {
-
-    let arr = [];
-
-    for (const key in ob) {
-        arr.push(key + "=" + ob[key]);
-    }
-
-    return arr.toString();
-}
-
-const orm = {
-    all: function (tableInput, cb) {
-        const queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-
-
-    create: function (table, cols, vals, cb) {
-        const queryString = "INSERT INTO " + table;
-
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-
-        console.log(queryString);
-
-        connection.query(queryString, vals, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-
-
-    update: function (table, objColVals, condition, cb) {
-        const queryString = "UPDATE " + table;
-
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    }
+  return arr.toString();
 };
+
+const objToSql = (ob) => {
+    const arr = [];
+  
+    for (const key in ob) {
+      let value = ob[key];
+  
+      if (Object.hasOwnProperty.call(ob, key)) {
+  
+        if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+          value = `'${value}'`;
+        }
+  
+        arr.push(`${key}=${value}`);
+      }
+    }
+  
+    // Translate array of strings to a single comma-separated string
+    return arr.toString();
+  };
+
+  const orm = {
+    all(tableInput, cb) {
+      const queryString = `SELECT * FROM ${tableInput};`;
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create(table, cols, vals, cb) {
+      let queryString = `INSERT INTO ${table}`;
+  
+      queryString += ' (';
+      queryString += cols.toString();
+      queryString += ') ';
+      queryString += 'VALUES (';
+      queryString += printQuestionMarks(vals.length);
+      queryString += ') ';
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+  
+    update(table, objColVals, condition, cb) {
+      let queryString = `UPDATE ${table}`;
+  
+      queryString += ' SET ';
+      queryString += objToSql(objColVals);
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    delete(table, condition, cb) {
+      let queryString = `DELETE FROM ${table}`;
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+  };
 
 module.exports = orm;
