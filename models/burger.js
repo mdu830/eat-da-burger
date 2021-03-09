dcf00744e16bf28ab3fedd1b5bea775c89dd4f48
mdu830 <mdu830@yahoo.com>
2021-03-09 11:49:07 -0500

    refactored burgers.js to es6 format

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,20 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    all: function (cb) {
+    all(cb) {
         orm.all("burgers", function (res) {
             cb(res);
         });
     },
-    create: function (name, cb) {
+    create(name, cb) {
         orm.create("burgers", [
             "burger_name", "devoured"
         ], [
             name, false
         ], cb);
     },
-    update: function (id, cb) {
-        var condition = "id=" + id;
+    update(id, cb) {
+        let condition = "id=" + id;
         orm.update("burgers", {
             devoured: true
         }, condition, cb);
