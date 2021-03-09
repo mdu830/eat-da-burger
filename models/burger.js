7934f7efa3c0a3b0affd88f1ad256ef717e77d0b
mdu830 <mdu830@yahoo.com>
2021-03-09 10:48:57 -0500

    finished working aplication

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+    create: function (name, cb) {
+        orm.create("burgers", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    update: function (id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+            devoured: true
+        }, condition, cb);
+    }
+};
+
+module.exports = burger;
---
+++ public/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $(".devour-form").on("submit", function (event) {
+        event.preventDefault();
+
+        const burger_id = $(this).children(".burger_id").val();
+        console.log(burger_id);
+        $.ajax({
+            method: "PUT",
+            url: "/burgers/" + burger_id
+        }).then(function (data) {
+
+            location.reload();
+        });
+
+    });
+});  
