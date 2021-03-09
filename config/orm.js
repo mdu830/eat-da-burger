9912b997244a4601275036077e14d606d5d1ea26
mdu830 <mdu830@yahoo.com>
2021-03-09 11:34:24 -0500

    updated orm.js (replaced var with const)

--- config/orm.js
+++ config/orm.js
@@ -4,7 +4,7 @@ function printQuestionMarks(num) {
     
     let arr = [];
 
-    for (var i = 0; i < num; i++) {
+    for (const i = 0; i < num; i++) {
         arr.push("?");
     }
 
@@ -22,9 +22,9 @@ function objToSql(ob) {
     return arr.toString();
 }
 
-var orm = {
+const orm = {
     all: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
+        const queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
@@ -35,7 +35,7 @@ var orm = {
 
 
     create: function (table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
+        const queryString = "INSERT INTO " + table;
 
         queryString += " (";
         queryString += cols.toString();
@@ -56,7 +56,7 @@ var orm = {
 
 
     update: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+        const queryString = "UPDATE " + table;
 
         queryString += " SET ";
         queryString += objToSql(objColVals);
